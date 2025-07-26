f5ef6f9554ac5e305222800dbd56484dd86cc464
josegvasconcelos <jgvasconcelos.dev@gmail.com>
2025-07-26 18:26:21 -0300

    feat: start with 2000 videos on the database

--- src/main/java/com/josegvasconcelos/videometadata/application/web/dto/response/LoginResponseDTO.java
+++ src/main/java/com/josegvasconcelos/videometadata/application/web/dto/response/LoginResponseDTO.java
@@ -2,5 +2,6 @@ package com.josegvasconcelos.videometadata.application.web.dto.response;
 
 import com.josegvasconcelos.videometadata.application.web.documentation.dto.response.LoginResponseDoc;
 
-public record LoginResponseDTO(String token) implements LoginResponseDoc {
-}
+public record LoginResponseDTO(
+        String token
+) implements LoginResponseDoc {}
